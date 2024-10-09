99e9ed73a4216d29db48edc24fac4188da3d6558
Jeric Nieva <jec.nieva@gmail.com>
2024-10-10 01:20:48 +0800

    Add project files.

---
+++ ifelse/Program.cs
@@ -0,0 +1,9 @@
+﻿using ifelse;
+
+class Program
+{
+     static void Main(string[] args)
+    {
+       Practice a = new Practice();
+    }
+}
---
+++ ifelse/practice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelse
+{
+     class Practice
+    {
+        public Practice() {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Random r = new Random();
+                int n = r.Next(1, 10);
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (n == a) { Console.Write("correct its "); }
+                else { Console.Write("wrong its "); }
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
